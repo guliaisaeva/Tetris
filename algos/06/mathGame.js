7fc0d3c63c0aa1eaab920b663eb61b4854aba54b
Gulia Isaeva <gulya.isaeva.pls@gmail.com>
2022-05-18 15:06:11 +0300

    algo 6 first attemts to solveo

--- algos/06/mathGame.js
+++ algos/06/mathGame.js
@@ -1,9 +1,17 @@
 const username = document.getElementById("userName");
 const addBtn = document.getElementById("button-addon2");
+const listBtn = document.getElementById("dropdownMenuButton1");
+const score = document.getElementById("score");
+const dropdown = document.getElementById("dropdown");
+const dropdownGroup = document.getElementById("dropdownGroup");
 
 function addName() {
   addBtn.addEventListener("click", function () {
-    // username.innerText = ;
-    document.getElementById("dropdownMenuButton1").innerHTML = `${username}`;
+    const name = username.value + "(0)";
+    listBtn.innerHTML = name;
+    let newBtn = document.createElement("button");
+    newBtn.innerHTML = name;
+    document.body.appendChild(newBtn);
   });
 }
+addName();
